309f9de9b6cf593cf9a61d5a248cc54df8aa3b35
uchachaduneli <uchachaduneli@gmail.com>
2019-11-27 01:24:06 +0400

    first commit

---
+++ src/main/java/org/openjfx/App.java
@@ -0,0 +1,51 @@
+package org.openjfx;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import org.openjfx.db.JpaUtil;
+
+import javax.persistence.EntityManager;
+import java.io.IOException;
+
+/**
+ * JavaFX App
+ */
+public class App extends Application {
+
+    private static Scene scene;
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        scene = new Scene(loadFXML("primary"));
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    static void setRoot(String fxml) throws IOException {
+        scene.setRoot(loadFXML(fxml));
+    }
+
+    private static Parent loadFXML(String fxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
+        return fxmlLoader.load();
+    }
+
+    public static void main(String[] args) {
+
+//        launch();
+
+        EntityManager entityManager = JpaUtil.getEntityManagerFactory().createEntityManager();
+        entityManager.getTransaction().begin();
+        // Check database version
+        String sql = "select version()";
+        String result = (String) entityManager.createNativeQuery(sql).getSingleResult();
+        System.out.println(result);
+        entityManager.getTransaction().commit();
+        entityManager.close();
+        JpaUtil.shutdown();
+    }
+
+}
